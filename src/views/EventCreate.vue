cc307411b595fe4f47eadb6bf076693eccb64b92
wcywin <wcywin@gmail.com>
2019-12-02 22:57:19 +0100

    add mapState and mapGetters to the store

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,10 +1,31 @@
 <template>
-    <h1>Create Event</h1>
+    <div>
+        <h1>Create Event, {{ user.name }}</h1>
+        <p>This event was created by {{ user.id }}</p>
+        <ul>
+            <li v-for="cat in categories" :key="cat">{{ cat }}</li>
+        </ul>
+        <p>There are {{ catLength }} categories</p>
+        <p>{{ getEvent(1) }}</p>
+        <p>{{ getEventById(2) }}</p>
+    </div>
+
 </template>
 
 <script>
+    import { mapState, mapGetters } from 'vuex'
 	export default {
-		name: "EventCreate"
+		name: "EventCreate",
+        computed: {
+			catLength() {
+				return this.$store.getters.catLength
+            },
+            getEvent() {
+				return this.$store.getters.getEventById
+            },
+            ...mapState(['user', 'categories']),
+            ...mapGetters(['getEventById'])
+        }
 	}
 </script>
 
